7be01670ee5478a936439d5027f043a35b969488
Pavel Azangue <155538939+raketa25@users.noreply.github.com>
2026-04-02 20:55:56 +0000

    Update interview codes

--- Linked Lists/inrterview_quests.py
+++ Linked Lists/inrterview_quests.py
@@ -51,6 +51,98 @@ class LinkedList:
             slow = slow.next
             fast = fast.next.next
         return slow.value
+    
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    
+    def partition_list(self, x):
+        dummy1 = Node(0)
+        dummy2 = Node(0)
+
+        prev1 = dummy1
+        prev2 = dummy2
+
+        temp = self.head
+        while temp:
+            if temp.value < x:
+                prev1.next = temp
+                prev1 = temp
+            else:
+                prev2.next = temp
+                prev2 = temp
+            temp = temp.next
+
+        prev1.next = dummy2.next
+        prev2.next = None
+
+        self.head = dummy1.next
+
+
+    def remove_duplicates(self):
+        temp = self.head
+        prev = None
+        seen_values = set()
+
+        while temp:
+            if temp.value in seen_values:
+                prev.next = temp.next
+            else:
+                seen_values.add(temp.value)
+                prev = temp
+            temp = temp.next
+
+
+    def binary_to_decima(self):
+        decimal_value = 0
+        temp = self.head
+
+        while temp:
+            decimal_value = decimal_value * 2 + temp.value
+            temp = temp.next
+        return decimal_value
+    
+
+    def reverse_between(self, start_index, end_index):
+        if start_index == end_index:
+            return
+
+        dummy = Node(0)
+        dummy.next = self.head
+        prev = dummy
+
+        for _ in range(start_index):
+            prev = prev.next
+
+        current = prev.next
+        for _ in range(end_index - start_index):
+            next_node = current.next
+            current.next = next_node.next
+            next_node.next = prev.next
+            prev.next = next_node
+
+        self.head = dummy.next
+    
+    def find_kth_from_end(ll, k):
+        slow = ll.head
+        fast = ll.head
+
+        for _ in range(k):
+            if fast is None:
+                return None
+            fast = fast.next
+
+        while fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow.value
 
 # ===================== Example usage ===================== #
 my_linked_list = LinkedList(1)
